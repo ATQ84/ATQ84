c099f35b42ad9fc42bb143e95663c4e543d1a4e4
San Se <101367491+atfork@users.noreply.github.com>
2022-05-02 20:36:45 +0800

    Update and rename li.js to l.js

---
+++ docs/l.js
@@ -0,0 +1,11 @@
+{
+  "number": 1, 
+  "array": [ ], 
+  "null": null, 
+  "string": "leo108", 
+  "boolean": true, 
+  "obj": { }, 
+  "level": {},
+  "value": "https://raw.githubusercontent.com/poduv/poduv/i/long",
+  "onclick": "CreateNewDoc()"
+}
