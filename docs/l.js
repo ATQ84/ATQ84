34fbe3df6dadf4bb51e5f234f390f3676936c848
San Se <101367491+atfork@users.noreply.github.com>
2022-05-02 18:18:18 +0800

    Update and rename urllist.js to l.js

---
+++ docs/l.js
@@ -0,0 +1,6 @@
+{
+  "a00001":"https://raw.githubusercontent.com/poduv/poduv/i/long",
+  "a00002":"https://raw.githubusercontent.com/poduv/poduv/i/long",
+  "a00005":"https://raw.githubusercontent.com/poduv/poduv/i/long"
+}
+
--- docs/urllist.js
+++
@@ -1,2 +0,0 @@
-{"a00001":"https://raw.githubusercontent.com/poduv/poduv/i/long","a00002":"https://raw.githubusercontent.com/poduv/poduv/i/long","a00005":"https://raw.githubusercontent.com/poduv/poduv/i/long"}
-
