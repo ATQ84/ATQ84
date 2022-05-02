f0094cb43b086696f1227de0727267ff3c338bd2
San Se <101367491+atfork@users.noreply.github.com>
2022-05-02 17:29:26 +0800

    Update urllist.js

--- docs/urllist.js
+++ docs/urllist.js
@@ -1,6 +1,2 @@
-{
-    "a00001":"https://raw.githubusercontent.com/poduv/poduv/i/long"
-    "a00002":"https://raw.githubusercontent.com/poduv/poduv/i/long"
-    "a00003":"https://raw.githubusercontent.com/poduv/poduv/i/long"
-    }
+{"a00001":"https://raw.githubusercontent.com/poduv/poduv/i/long","a00002":"https://raw.githubusercontent.com/poduv/poduv/i/long","a00003":"https://raw.githubusercontent.com/poduv/poduv/i/long"}
 
